19c933cff9f69c991492d2ee622488d9f5495a08
Oliver Braun <ob@cs.hm.edu>
2023-06-13 07:25:54 +0200

    request rooms

--- cmd/info.go
+++ cmd/info.go
@@ -13,14 +13,20 @@ var (
 		Use:   "info [subcommand]",
 		Short: "get info",
 		Long: `Get info.
-goslots --- info about slots for GO/GN
-stats --- get statistics.`,
+goslots       --- info about slots for GO/GN
+request-rooms --- which rooms to request
+stats         --- get statistics.`,
 		Args: cobra.MinimumNArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
 			p := initPlexamsConfig()
 			switch args[0] {
 			case "goslots":
 				err := plexams.PrintGOSlots(p.GetSemesterConfig().Slots, p.GetGoSlots())
+				if err != nil {
+					log.Fatalf("got error: %v\n", err)
+				}
+			case "request-rooms":
+				err := p.RequestRooms()
 				if err != nil {
 					log.Fatalf("got error: %v\n", err)
 				}
---
+++ plexams/request_rooms.go
@@ -0,0 +1,121 @@
+package plexams
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/rs/zerolog/log"
+)
+
+func (p *Plexams) RequestRooms() error {
+	ctx := context.Background()
+	// globalRooms, err := p.dbClient.GlobalRooms(ctx)
+	// if err != nil {
+	// 	log.Error().Err(err).Msg("cannot get global rooms")
+	// 	return err
+	// }
+
+	// dayNumber -> slotNumber -> number of needed rooms
+	needBigRooms := make(map[int]map[int]int)
+
+	for _, day := range p.semesterConfig.Days {
+		needBigRooms[day.Number] = make(map[int]int)
+	}
+
+	for _, slot := range p.semesterConfig.Slots {
+		examsInSlot, err := p.ExamsInSlot(ctx, slot.DayNumber, slot.SlotNumber)
+		if err != nil {
+			log.Error().Err(err).Int("day", slot.DayNumber).Int("slot", slot.SlotNumber).Msg("cannot get exams in slot")
+			return err
+		}
+
+		for _, exam := range examsInSlot {
+			if exam.Constraints != nil && exam.Constraints.RoomConstraints != nil &&
+				(exam.Constraints.RoomConstraints.ExahmRooms || exam.Constraints.RoomConstraints.Lab ||
+					exam.Constraints.RoomConstraints.Seb || exam.Constraints.RoomConstraints.PlacesWithSocket) {
+				continue
+			}
+
+			reqs := 0
+			for _, req := range exam.Exam.StudentRegs {
+				reqs += len(req.StudentRegs)
+			}
+
+			if reqs >= 30 {
+				needBigRooms[slot.DayNumber][slot.SlotNumber]++
+			}
+			if reqs >= 85 {
+				needBigRooms[slot.DayNumber][slot.SlotNumber]++
+			}
+		}
+	}
+
+	// print for plexams.yaml
+	fmt.Println("  R1.046:\n    allowedSlots:")
+	for _, slot := range p.semesterConfig.Slots {
+		if needBigRooms[slot.DayNumber][slot.SlotNumber] > 0 {
+			fmt.Printf("      - [%d,%d] # %s, %s\n", slot.DayNumber, slot.SlotNumber,
+				p.semesterConfig.Days[slot.DayNumber-1].Date.Format("02.01.06"), slot.Starttime.Format("15:04"))
+		}
+	}
+	fmt.Println("  R1.049:\n    allowedSlots:")
+	for _, slot := range p.semesterConfig.Slots {
+		if needBigRooms[slot.DayNumber][slot.SlotNumber] > 1 {
+			fmt.Printf("      - [%d,%d] # %s, %s\n", slot.DayNumber, slot.SlotNumber,
+				p.semesterConfig.Days[slot.DayNumber-1].Date.Format("02.01.06"), slot.Starttime.Format("15:04"))
+		}
+	}
+
+	// print dates and times for request
+	fmt.Println("- R1.046")
+	for _, day := range p.semesterConfig.Days {
+		if len(needBigRooms[day.Number]) == 0 {
+			continue
+		}
+		fmt.Printf("  - %s\n", day.Date.Format("02.01.06"))
+		for i, slot := range p.semesterConfig.Starttimes {
+			if needBigRooms[day.Number][i+1] > 0 {
+				starttime, err := time.Parse("15:04", slot.Start)
+				if err != nil {
+					log.Error().Err(err).Str("time-string", slot.Start).Msg("cannot parse time")
+					return err
+				}
+				fmt.Printf("    - %v - %v\n",
+					starttime.Add(-30*time.Minute).Format("15:04"),
+					starttime.Add(120*time.Minute).Format("15:04"))
+			}
+		}
+	}
+	fmt.Println("- R1.049")
+	for _, day := range p.semesterConfig.Days {
+		if len(needBigRooms[day.Number]) == 0 {
+			continue
+		}
+		needDay := false
+		for i := range p.semesterConfig.Starttimes {
+			if needBigRooms[day.Number][i+1] > 1 {
+				needDay = true
+			}
+		}
+		if !needDay {
+			continue
+		}
+
+		fmt.Printf("  - %s\n", day.Date.Format("02.01.06"))
+		for i, slot := range p.semesterConfig.Starttimes {
+			if needBigRooms[day.Number][i+1] > 1 {
+				starttime, err := time.Parse("15:04", slot.Start)
+				if err != nil {
+					log.Error().Err(err).Str("time-string", slot.Start).Msg("cannot parse time")
+					return err
+				}
+				fmt.Printf("    - %v - %v\n",
+					starttime.Add(-30*time.Minute).Format("15:04"),
+					starttime.Add(120*time.Minute).Format("15:04"))
+			}
+		}
+	}
+
+	return nil
+}

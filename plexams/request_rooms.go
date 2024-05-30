1b4e2d185ec3757e858acd5e967b888bc9c173dc
Oliver Braun <ob@cs.hm.edu>
2024-05-30 15:41:17 +0200

    adapt request rooms to new data structure

--- plexams/request_rooms.go
+++ plexams/request_rooms.go
@@ -24,23 +24,23 @@ func (p *Plexams) RequestRooms() error {
 	}
 
 	for _, slot := range p.semesterConfig.Slots {
-		examsInSlot, err := p.ExamsInSlot(ctx, slot.DayNumber, slot.SlotNumber)
+		examsInSlot, err := p.GetExamsInSlot(ctx, slot.DayNumber, slot.SlotNumber)
 		if err != nil {
 			log.Error().Err(err).Int("day", slot.DayNumber).Int("slot", slot.SlotNumber).Msg("cannot get exams in slot")
 			return err
 		}
 
 		for _, exam := range examsInSlot {
+			if exam.Constraints != nil && exam.Constraints.NotPlannedByMe {
+				continue
+			}
 			if exam.Constraints != nil && exam.Constraints.RoomConstraints != nil &&
 				(exam.Constraints.RoomConstraints.ExahmRooms || exam.Constraints.RoomConstraints.Lab ||
 					exam.Constraints.RoomConstraints.Seb || exam.Constraints.RoomConstraints.PlacesWithSocket) {
 				continue
 			}
 
-			reqs := 0
-			for _, req := range exam.Exam.StudentRegs {
-				reqs += len(req.StudentRegs)
-			}
+			reqs := exam.StudentRegsCount
 
 			if reqs >= 30 {
 				needBigRooms[slot.DayNumber][slot.SlotNumber]++
@@ -52,28 +52,52 @@ func (p *Plexams) RequestRooms() error {
 	}
 
 	// print for plexams.yaml
-	fmt.Println("  R1.046:\n    allowedSlots:")
+	fmt.Println("  R1.046:\n    reservations:")
 	for _, slot := range p.semesterConfig.Slots {
 		if needBigRooms[slot.DayNumber][slot.SlotNumber] > 0 {
-			fmt.Printf("      - [%d,%d] # %s, %s\n", slot.DayNumber, slot.SlotNumber,
-				p.semesterConfig.Days[slot.DayNumber-1].Date.Format("02.01.06"), slot.Starttime.Format("15:04"))
+			noOfRooms := "einen angefragt"
+			if needBigRooms[slot.DayNumber][slot.SlotNumber] > 1 {
+				noOfRooms = "beide angefragt"
+			}
+			fmt.Printf(`      - slot: [%d,%d] # %s
+        date: %s
+        from: %s
+        until: %s
+        approved: false
+`,
+				slot.DayNumber, slot.SlotNumber, noOfRooms,
+				p.semesterConfig.Days[slot.DayNumber-1].Date.Format("2006-01-02"),
+				slot.Starttime.Add(-15*time.Minute).Format("15:04"),
+				slot.Starttime.Add(105*time.Minute).Format("15:04"))
 		}
 	}
-	fmt.Println("  R1.049:\n    allowedSlots:")
+	fmt.Println("  R1.049:\n    reservations:")
 	for _, slot := range p.semesterConfig.Slots {
 		if needBigRooms[slot.DayNumber][slot.SlotNumber] > 1 {
-			fmt.Printf("      - [%d,%d] # %s, %s\n", slot.DayNumber, slot.SlotNumber,
-				p.semesterConfig.Days[slot.DayNumber-1].Date.Format("02.01.06"), slot.Starttime.Format("15:04"))
+			noOfRooms := "einen angefragt"
+			if needBigRooms[slot.DayNumber][slot.SlotNumber] > 1 {
+				noOfRooms = "beide angefragt"
+			}
+			fmt.Printf(`      - slot: [%d,%d] # %s
+        date: %s
+        from: %s
+        until: %s
+        approved: false
+`,
+				slot.DayNumber, slot.SlotNumber, noOfRooms,
+				p.semesterConfig.Days[slot.DayNumber-1].Date.Format("2006-01-02"),
+				slot.Starttime.Add(-15*time.Minute).Format("15:04"),
+				slot.Starttime.Add(105*time.Minute).Format("15:04"))
 		}
 	}
 
 	// print dates and times for request
-	fmt.Println("- R1.046")
+	fmt.Println("Für E-Mail-Anfrage:")
 	for _, day := range p.semesterConfig.Days {
 		if len(needBigRooms[day.Number]) == 0 {
 			continue
 		}
-		fmt.Printf("  - %s\n", day.Date.Format("02.01.06"))
+		fmt.Printf("- %s\n", day.Date.Format("02.01.06"))
 		for i, slot := range p.semesterConfig.Starttimes {
 			if needBigRooms[day.Number][i+1] > 0 {
 				starttime, err := time.Parse("15:04", slot.Start)
@@ -81,41 +105,46 @@ func (p *Plexams) RequestRooms() error {
 					log.Error().Err(err).Str("time-string", slot.Start).Msg("cannot parse time")
 					return err
 				}
-				fmt.Printf("    - %v - %v\n",
-					starttime.Add(-30*time.Minute).Format("15:04"),
-					starttime.Add(120*time.Minute).Format("15:04"))
-			}
-		}
-	}
-	fmt.Println("- R1.049")
-	for _, day := range p.semesterConfig.Days {
-		if len(needBigRooms[day.Number]) == 0 {
-			continue
-		}
-		needDay := false
-		for i := range p.semesterConfig.Starttimes {
-			if needBigRooms[day.Number][i+1] > 1 {
-				needDay = true
-			}
-		}
-		if !needDay {
-			continue
-		}
-
-		fmt.Printf("  - %s\n", day.Date.Format("02.01.06"))
-		for i, slot := range p.semesterConfig.Starttimes {
-			if needBigRooms[day.Number][i+1] > 1 {
-				starttime, err := time.Parse("15:04", slot.Start)
-				if err != nil {
-					log.Error().Err(err).Str("time-string", slot.Start).Msg("cannot parse time")
-					return err
+				fmt.Printf("  - %v - %v Uhr: ",
+					starttime.Add(-15*time.Minute).Format("15:04"),
+					starttime.Add(105*time.Minute).Format("15:04"))
+				if needBigRooms[day.Number][i+1] > 1 {
+					fmt.Println("beide (R1.046 und R1.049)")
+				} else {
+					fmt.Println("einen (entweder R1.046 oder R1.049)")
 				}
-				fmt.Printf("    - %v - %v\n",
-					starttime.Add(-30*time.Minute).Format("15:04"),
-					starttime.Add(120*time.Minute).Format("15:04"))
 			}
 		}
 	}
+	// fmt.Println("- R1.049")
+	// for _, day := range p.semesterConfig.Days {
+	// 	if len(needBigRooms[day.Number]) == 0 {
+	// 		continue
+	// 	}
+	// 	needDay := false
+	// 	for i := range p.semesterConfig.Starttimes {
+	// 		if needBigRooms[day.Number][i+1] > 1 {
+	// 			needDay = true
+	// 		}
+	// 	}
+	// 	if !needDay {
+	// 		continue
+	// 	}
+
+	// 	fmt.Printf("  - %s\n", day.Date.Format("02.01.06"))
+	// 	for i, slot := range p.semesterConfig.Starttimes {
+	// 		if needBigRooms[day.Number][i+1] > 1 {
+	// 			starttime, err := time.Parse("15:04", slot.Start)
+	// 			if err != nil {
+	// 				log.Error().Err(err).Str("time-string", slot.Start).Msg("cannot parse time")
+	// 				return err
+	// 			}
+	// 			fmt.Printf("    - %v - %v\n",
+	// 				starttime.Add(-15*time.Minute).Format("15:04"),
+	// 				starttime.Add(105*time.Minute).Format("15:04"))
+	// 		}
+	// 	}
+	// }
 
 	return nil
 }

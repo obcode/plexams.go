b641bb41ab03852091bbba36ad262e3d1639faa0
Oliver Braun <ob@cs.hm.edu>
2024-11-28 15:18:54 +0100

    connected exams with non zpa exams

--- db/non_zpa_exams.go
+++ db/non_zpa_exams.go
@@ -2,11 +2,42 @@ package db
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/obcode/plexams.go/graph/model"
+	"github.com/rs/zerolog/log"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 func (db *DB) AddNonZpaExam(ctx context.Context, exam *model.ZPAExam) error {
 	_, err := db.Client.Database(db.databaseName).Collection(collectionNonZpaExams).InsertOne(ctx, exam)
 	return err
+}
+
+func (db *DB) NonZpaExam(ctx context.Context, ancode int) (*model.ZPAExam, error) {
+	var exam model.ZPAExam
+	err := db.Client.Database(db.databaseName).Collection(collectionNonZpaExams).FindOne(ctx, bson.M{"ancode": ancode}).Decode(&exam)
+	if err != nil {
+		log.Error().Err(err).Msg("cannot get non zpa exam")
+		return nil, err
+	}
+	return &exam, nil
+}
+
+func (db *DB) NonZpaExams(ctx context.Context) ([]*model.ZPAExam, error) {
+	var exams []*model.ZPAExam
+	cur, err := db.Client.Database(db.databaseName).Collection(collectionNonZpaExams).Find(ctx, bson.M{})
+	if err != nil {
+		log.Error().Err(err).Msg("cannot get non zpa exams")
+		return nil, err
+	}
+	defer cur.Close(ctx)
+
+	err = cur.All(ctx, &exams)
+	if err != nil {
+		log.Error().Err(err).Msg("cannot get non zpa exams")
+		return nil, err
+	}
+	fmt.Printf("exams: %v\n", exams)
+	return exams, nil
 }

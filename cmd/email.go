c0696ac67b52dea3897e2ae0d99decd0b0c825d6
Oliver Braun <ob@cs.hm.edu>
2024-11-29 12:40:01 +0100

    feat: send email about just one ancode

--- cmd/email.go
+++ cmd/email.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"os"
+	"strconv"
 
 	"github.com/spf13/cobra"
 )
@@ -15,26 +17,37 @@ var (
 		Long: `Send emails.
 nta --- send emails to teachers about nta,
 nta-with-room-alone --- send emails to students with room alone,
-primuss-data --- send emails to teachers about primuss data and nta.`,
+primuss-data [all|<ancode>] --- send emails to teachers about primuss data and nta.`,
 		Args: cobra.MinimumNArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
 			plexams := initPlexamsConfig()
 			switch args[0] {
-			// case "nta":
-			// 	err := plexams.SendHandicapsMails(context.Background(), run)
-			// 	if err != nil {
-			// 		log.Fatalf("got error: %v\n", err)
-			// 	}
 			case "nta-with-room-alone":
 				err := plexams.SendHandicapsMailsNTARoomAlone(context.Background(), run)
 				if err != nil {
 					log.Fatalf("got error: %v\n", err)
 				}
 			case "primuss-data":
-				err := plexams.SendGeneratedExamMails(context.Background(), run)
-				if err != nil {
-					log.Fatalf("got error: %v\n", err)
+				if len(args) < 2 {
+					log.Fatal("need program and primuss-ancode")
 				}
+				if args[1] == "all" {
+					err := plexams.SendGeneratedExamMails(context.Background(), run)
+					if err != nil {
+						log.Fatalf("got error: %v\n", err)
+					}
+				} else {
+					ancode, err := strconv.Atoi(args[1])
+					if err != nil {
+						fmt.Printf("cannot use %s as ancode", args[1])
+						os.Exit(1)
+					}
+					err = plexams.SendGeneratedExamMail(context.Background(), ancode, run)
+					if err != nil {
+						log.Fatalf("got error: %v\n", err)
+					}
+				}
+
 			default:
 				fmt.Println("email called with unknown sub command")
 			}

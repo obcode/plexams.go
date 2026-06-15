757573573cccd830e9417235b2d26a225bf52905
Oliver Braun <ob@cs.hm.edu>
2026-06-15 20:03:18 +0200

    feat: pre-planing invigilations

--- cmd/invigilation.go
+++ cmd/invigilation.go
@@ -16,7 +16,10 @@ var (
 		Short: "Add an invigilation",
 		Long: `Add an invigilation.
 reserve    [daynumber] [slotnumber] [invigilator ID] --- add reserve for slot (daynumber,slotnumber).
-[roomname] [daynumber] [slotnumber] [invigilator ID] --- add invigilator for room in slot`,
+[roomname] [daynumber] [slotnumber] [invigilator ID] --- add invigilator for room in slot
+
+With --pre-plan/-p the invigilation is pre-planned instead of added, i.e. it is
+stored as a fixed assignment that the automatic invigilation planning respects.`,
 		Args: cobra.MinimumNArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
 			plexams := initPlexamsConfig()
@@ -81,16 +84,36 @@ reserve    [daynumber] [slotnumber] [invigilator ID] --- add reserve for slot (d
 				os.Exit(1)
 			}
 
+			verb := "Add"
+			if prePlanInvigilation {
+				verb = "Pre-plan"
+			}
 			if oldInvigilator != nil {
-				if !confirm(fmt.Sprintf("Add \"%s\" and override existing invigilator \"%s\" in slot (%d,%d) for \"%s\"?",
-					newInvigilator.Teacher.Shortname, oldInvigilator.Shortname, day, slot, room), 1) {
+				if !confirm(fmt.Sprintf("%s \"%s\" and override existing invigilator \"%s\" in slot (%d,%d) for \"%s\"?",
+					verb, newInvigilator.Teacher.Shortname, oldInvigilator.Shortname, day, slot, room), 1) {
 					os.Exit(0)
 				}
 			} else {
-				if !confirm(fmt.Sprintf("Add \"%s\" for \"%s\" in slot (%d,%d)?",
-					newInvigilator.Teacher.Shortname, room, day, slot), 1) {
+				if !confirm(fmt.Sprintf("%s \"%s\" for \"%s\" in slot (%d,%d)?",
+					verb, newInvigilator.Teacher.Shortname, room, day, slot), 1) {
 					os.Exit(0)
 				}
+			}
+
+			if prePlanInvigilation {
+				var roomName *string
+				if room != "reserve" {
+					roomName = &room
+				}
+				success, err := plexams.PreAddInvigilation(context.Background(), invigilatorID, day, slot, roomName)
+				if err != nil {
+					log.Fatalf("got error: %v\n", err)
+				}
+				if success {
+					fmt.Printf("successfully pre-planned \"%s\" for \"%s\" in slot (%d,%d)\n",
+						newInvigilator.Teacher.Shortname, room, day, slot)
+				}
+				return
 			}
 
 			err = plexams.AddInvigilation(context.Background(), room, day, slot, invigilatorID)
@@ -107,8 +130,10 @@ reserve    [daynumber] [slotnumber] [invigilator ID] --- add reserve for slot (d
 
 		},
 	}
+	prePlanInvigilation bool
 )
 
 func init() {
 	rootCmd.AddCommand(invigilationCmd)
+	invigilationCmd.Flags().BoolVarP(&prePlanInvigilation, "pre-plan", "p", false, "pre-plan the invigilation instead of adding it")
 }

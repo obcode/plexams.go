83f5bed8575dfa0471e3724c38719892bd579ccb
Oliver Braun <ob@cs.hm.edu>
2026-07-09 22:12:59 +0200

    refactor(db): extract collection name retrieval to a separate function

--- db/save.go
+++ db/save.go
@@ -6,21 +6,32 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
+func collectionNameFromContext(ctx context.Context) string {
+	if name, ok := ctx.Value(CollectionName("collectionName")).(string); ok {
+		return name
+	}
+	return ""
+}
+
 func (db *DB) DropAndSave(ctx context.Context, objects []interface{}) error {
 	collection := db.getCollectionSemesterFromContext(ctx)
 
 	err := collection.Drop(ctx)
 	if err != nil {
 		log.Error().Err(err).
-			Str("collectionName", ctx.Value("collectionName").(string)).
+			Str("collectionName", collectionNameFromContext(ctx)).
 			Msg("cannot drop collection")
 		return err
+	}
+
+	if len(objects) == 0 {
+		return nil
 	}
 
 	_, err = collection.InsertMany(ctx, objects)
 	if err != nil {
 		log.Error().Err(err).
-			Str("collectionName", ctx.Value("collectionName").(string)).
+			Str("collectionName", collectionNameFromContext(ctx)).
 			Msg("cannot insert objects")
 		return err
 	}
@@ -29,12 +40,16 @@ func (db *DB) DropAndSave(ctx context.Context, objects []interface{}) error {
 }
 
 func (db *DB) Save(ctx context.Context, objects []interface{}) error {
+	if len(objects) == 0 {
+		return nil
+	}
+
 	collection := db.getCollectionSemesterFromContext(ctx)
 
 	_, err := collection.InsertMany(ctx, objects)
 	if err != nil {
 		log.Error().Err(err).
-			Str("collectionName", ctx.Value("collectionName").(string)).
+			Str("collectionName", collectionNameFromContext(ctx)).
 			Msg("cannot insert objects")
 		return err
 	}
